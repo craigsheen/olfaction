e61af2659a6bbda4185adf0a6231c29bdecd7514
Craig Sheen <craig_sheen@hotmail.com>
2016-01-20 09:44:04 +1100

    Skip CSRF check for API call

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,4 +1,6 @@
 class PullRequestsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def create
     Rails.logger.debug params
     if params[:action] == "opened"
