59d78aa2090e167d0b03e1fa81960a10254d533f
Craig Sheen <craig_sheen@hotmail.com>
2016-01-20 09:47:40 +1100

    Use strings instead of symbols for hash

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,12 +1,13 @@
 class PullRequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  
+
   def create
-    Rails.logger.debug params
-    if params[:action] == "opened"
-      repo = params[:repository][:name]
-      id = params[:number]
-      CheckPullRequest.new(repo, id).run
+    if params["action"] == "opened"
+      repo = params["repository"]["name"]
+      id = params["number"]
+      checker = CheckPullRequest.new(repo, id)
+      Rails.logger.debug checker
+      checker.run
     end
   end
 end
