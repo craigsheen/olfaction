24c6d38ca52cfde6803f41e472450005d8600794
Craig Sheen <craig_sheen@hotmail.com>
2016-01-20 19:39:07 +1100

    Interpolate ENV['access_token'] in key_vault file

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -2,10 +2,12 @@ class PullRequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    repo = params["repository"]["full_name"]
-    id = params["number"]
-    checker = CheckPullRequest.new(repo, id)
-    checker.run
+    if params["action"] == "created"
+      repo = params["repository"]["full_name"]
+      id = params["number"]
+      checker = CheckPullRequest.new(repo, id)
+      checker.run
+    end
     render json: {}, status: 200
   end
 end
